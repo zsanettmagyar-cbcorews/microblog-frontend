028a219b4170ef37c2ee701b2db4e80ada9663d9
Logan Donley <ldonley@cloudbees.com>
2019-12-19 12:07:57 -0500

    Cleaning up code. [SAO-88]

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -2,16 +2,16 @@ import Rox from 'rox-browser'
 
 export const Flags = {
   sidebar: new Rox.Flag(false)
-}
+};
 
 export const configurationFetchedHandler = fetcherResults => {
   if (fetcherResults.hasChanges && fetcherResults.fetcherStatus === 'APPLIED_FROM_NETWORK') {
     window.location.reload(false)
   }
-}
+};
 const options = {
   configurationFetchedHandler: configurationFetchedHandler
-}
+};
 
-Rox.register('default', Flags)
-Rox.setup(process.env.VUE_APP_ROLLOUT_KEY, options)
+Rox.register('default', Flags);
+Rox.setup(process.env.VUE_APP_ROLLOUT_KEY, options);
