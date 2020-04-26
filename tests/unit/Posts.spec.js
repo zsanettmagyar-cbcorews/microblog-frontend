fbc102bcab4f47195c302f3fab89d86a48f51810
Kurt Madel <kmadel@cloudbees.com>
2020-04-26 08:11:36 -0400

    Update Posts.spec.js
    commit intentional typo for lab

--- tests/unit/Posts.spec.js
+++ tests/unit/Posts.spec.js
@@ -2,7 +2,7 @@ import Posts from '@/views/Posts.vue'
 
 describe('Posts.vue', () => {
   it('has a created hook', () => {
-    expect(typeof Posts.created).toBe('function')
+    expect(typeof Posts.created).toBe('funcion')
   })
   it('sets the correct default data', () => {
     expect(typeof Posts.data).toBe('function')
