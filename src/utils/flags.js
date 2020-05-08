32dfdd494b703fccfc46c7029744e21435aa1b6a
elcloudbees <55796599+elcloudbees@users.noreply.github.com>
2020-05-07 22:54:25 -0500

    Update flags.js

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -4,15 +4,15 @@ export const Flags = {
   sidebar: new Rox.Flag(false),
   flagEnabled: new Rox.Flag(false)
 };
-/*Can comment out for Lab 1
+Can comment out for Lab 1
 export const configurationFetchedHandler = fetcherResults => {
   if (fetcherResults.hasChanges && fetcherResults.fetcherStatus === 'APPLIED_FROM_NETWORK') {
     window.location.reload(false)
   }
 };
-*/
+
 const options = {
-  /*configurationFetchedHandler: configurationFetchedHandler*/
+  configurationFetchedHandler: configurationFetchedHandler
 };
 
 Rox.register('default', Flags);
