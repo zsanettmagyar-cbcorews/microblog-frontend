f4e7cea5ee9f8b3c34d8b6314aadd8a4e1ab14c2
elcloudbees <55796599+elcloudbees@users.noreply.github.com>
2020-05-07 22:55:37 -0500

    Update flags.js

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -4,7 +4,7 @@ export const Flags = {
   sidebar: new Rox.Flag(false),
   flagEnabled: new Rox.Flag(false)
 };
-Can comment out for Lab 1
+
 export const configurationFetchedHandler = fetcherResults => {
   if (fetcherResults.hasChanges && fetcherResults.fetcherStatus === 'APPLIED_FROM_NETWORK') {
     window.location.reload(false)
