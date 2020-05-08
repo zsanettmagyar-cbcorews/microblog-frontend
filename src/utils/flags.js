9b8313e03fa32ac54cf8d1e73c39fc42c652f2d7
elcloudbees <55796599+elcloudbees@users.noreply.github.com>
2020-05-08 09:14:43 -0500

    Update flags.js

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -2,7 +2,7 @@ import Rox from 'rox-browser'
 
 export const Flags = {
   sidebar: new Rox.Flag(false),
-  flagEnabled: new Rox.Flag(false)
+  title: new Rox.Flag(false)
 };
 
 export const configurationFetchedHandler = fetcherResults => {
