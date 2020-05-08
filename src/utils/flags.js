595622ade720386f9fd1f1a09ead87f5d35c981f
elcloudbees <55796599+elcloudbees@users.noreply.github.com>
2020-05-08 12:06:49 -0500

    Update flags.js

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -1,22 +1,11 @@
 import Rox from 'rox-browser'
-import store from '../store'
 
 export const Flags = {
-  sidebar: new Rox.Flag(false),
-  title: new Rox.Flag(false)
-};
-
-export const configurationFetchedHandler = fetcherResults => {
-  if (fetcherResults.hasChanges && fetcherResults.fetcherStatus === 'APPLIED_FROM_NETWORK') {
-    window.location.reload(false)
-  }
+  sidebar: new Rox.Flag(false)
 };
 
 const options = {
-  configurationFetchedHandler: configurationFetchedHandler
 };
-
-Rox.setCustomBooleanProperty('isLoggedIn', store.getters.isLoggedIn);
 
 Rox.register('default', Flags);
 Rox.setup(process.env.VUE_APP_ROLLOUT_KEY, options);
