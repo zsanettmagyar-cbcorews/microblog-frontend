2264fde200e95c66e52c2097613d50b060e2c8d0
Zsanett Magyar <mzsanett92@gmail.com>
2020-05-20 16:29:55 +0200

    Update flags.js
    Add configuration fetched handler

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -5,7 +5,18 @@ export const Flags = {
   title: new Rox.Flag(false)
 };
 
+export const configurationFetchedHandler = fetcherResults => {
+  console.log('The configuration status is: ' + fetcherResults.fetcherStatus)
+  if (fetcherResults.hasChanges && fetcherResults.fetcherStatus === 'APPLIED_FROM_NETWORK') {
+    window.location.reload(false)
+  }
+  else if (fetcherResults.fetcherStatus === 'ERROR_FETCH_FAILED') {
+    console.log('Error occured! Details are: ' + fetcherResults.errorDetails)
+  }
+};
+
 const options = {
+  configurationFetchedHandler: configurationFetchedHandler
 };
 
 Rox.register('default', Flags);
